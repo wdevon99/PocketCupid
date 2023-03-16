3d4eda4f47d0b8e7651cbcab0f5410ccf1ce5303
yasiruperera123 <yasirup@99x.io>
2023-03-16 23:28:43 +0530

    Update Intro Views

---
+++ src/constants/imageConstants.js
@@ -0,0 +1,5 @@
+export const Images = {
+  illastrater1: require('../assets/illastrater1.png'),
+  illastrater2: require('../assets/illastrater2.png'),
+  illastrater3: require('../assets/illastrater3.png'),
+};
