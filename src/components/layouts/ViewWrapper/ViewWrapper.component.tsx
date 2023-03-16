16c57f4d24a7acd1ba5c58617486930f49328636
yasiruperera123 <yasirup@99x.io>
2023-03-16 15:41:59 +0530

    Update viewWrapper and Intro screen

--- src/components/layouts/ViewWrapper/ViewWrapper.component.tsx
+++ src/components/layouts/ViewWrapper/ViewWrapper.component.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react';
 import type {PropsWithChildren} from 'react';
-import {SafeAreaView, useColorScheme, StatusBar, View} from 'react-native';
+import {
+  SafeAreaView,
+  useColorScheme,
+  StatusBar,
+  View,
+  ImageBackground,
+} from 'react-native';
 import Colors from '../../../styles/colors';
 import styles from './ViewWrapper.style';
 
@@ -10,13 +16,19 @@ const ViewWrapper: React.FC<PropsWithChildren> = ({children}) => {
     backgroundColor: isDarkMode ? Colors.BLACK : Colors.WHITE,
   };
   return (
-    <SafeAreaView style={[styles.wrapper]}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <View style={[styles.childWrapper]}>{children}</View>
-    </SafeAreaView>
+    <ImageBackground
+      resizeMode="cover"
+      style={[styles.childWrapper]}
+      source={require('../../../assets/blur.png')}>
+      <SafeAreaView style={[styles.wrapper]}>
+        <StatusBar
+          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={backgroundStyle.backgroundColor}
+        />
+
+        <View style={[styles.childWrapper]}>{children}</View>
+      </SafeAreaView>
+    </ImageBackground>
   );
 };
 
